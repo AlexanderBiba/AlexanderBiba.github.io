20f904be5fe7caab7dc604105c0a36afdeabf6f3
Alex Biba <alexbiba@gmail.com>
2022-05-29 22:34:26 -0400

    Init

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <header>
+            <h1>I'm Alex Biba</h1>
+            <h2>I'm a New York based developer</h2>
+        </header>
+    );
+}
