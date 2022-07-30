fa3a1589b1e6a906ff3cf220e64ed7229b6ae3c1
Alex Biba <alexbiba@gmail.com>
2022-07-30 09:26:20 -0400

    Update Works.js

--- src/Works.js
+++ src/Works.js
@@ -7,8 +7,8 @@ export default function Works() {
             <div className="content">
                 <h3><a className="project" href="https://alex152.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/alex152/wordle" target="_blank"/></h3>
                 <h4>A simple Wordle game with a new word every day</h4>
-                <h3><a className="project" href="https://alex152.github.io/goal-buddy">Goal Buddy</a><a className="fa fa-github" href="https://github.com/alex152/goal-buddy" target="_blank"/></h3>
-                <h4>Set goals for tomorrow and have them reviewed by your buddy</h4>
+                <h3><a className="project" href="https://alex152.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/alex152/goal-keeper" target="_blank"/></h3>
+                <h4>A ToDo List With a Twist!</h4>
             </div>
         </section>
     )
