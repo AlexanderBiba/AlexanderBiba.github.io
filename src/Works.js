37a10b61acf0cdf5f924c93998cb8a22a483c5d4
Alex Biba <alexbiba@gmail.com>
2022-07-30 09:29:30 -0400

    Update works

--- src/Works.js
+++ src/Works.js
@@ -6,9 +6,11 @@ export default function Works() {
             <h2 className="title">Works</h2>
             <div className="content">
                 <h3><a className="project" href="https://alex152.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/alex152/wordle" target="_blank"/></h3>
-                <h4>A simple Wordle game with a new word every day</h4>
+                <h4>A Classic Wordle Game With a New Word Every Day</h4>
                 <h3><a className="project" href="https://alex152.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/alex152/goal-keeper" target="_blank"/></h3>
                 <h4>A ToDo List With a Twist!</h4>
+                <h3><a className="project" href="https://github.com/alex152/password-generator">Python Password Generator</a><a className="fa fa-github" href="https://github.com/alex152/password-generator" target="_blank"/></h3>
+                <h4>A Password Generator Written in Python</h4>
             </div>
         </section>
     )
