86e7f687902470a309f65457c94e56329506ab68
Alex Biba <alexbiba@gmail.com>
2022-07-31 20:42:18 -0400

    Update experience to Ps

--- src/Experience.js
+++ src/Experience.js
@@ -8,22 +8,22 @@ export default function Experience() {
                 <div>
                     <h3>Bloomberg, New York</h3>
                     <h4>2018-2022</h4>
-                    <h5>Developing Equity Research Document Search Engine and a Collaborative Document Viewer Web Application</h5>
+                    <p>Developing Equity Research Document Search Engine and a Collaborative Document Viewer Web Application</p>
                 </div>
                 <div>
                     <h3>Bloomberg, Tel Aviv</h3>
                     <h4>2016-2018</h4>
-                    <h5>Developing Bloomberg's Transaction Cost Analysis (BTCA) Terminal Application</h5>
+                    <p>Developing Bloomberg's Transaction Cost Analysis (BTCA) Terminal Application</p>
                 </div>
                 <div>
                     <h3>Intel, Haifa</h3>
                     <h4>2015-2016</h4>
-                    <h5>Developing pre-silicon hardware verification enviroments for an image processing SoC</h5>
+                    <p>Developing pre-silicon hardware verification enviroments for an image processing SoC</p>
                 </div>
                 <div>
                     <h3>CSR (acquired by Qualcomm), Haifa</h3>
                     <h4>2012-2015</h4>
-                    <h5>Developing pre-silicon hardware verification enviroments for automotive SoCs</h5>
+                    <p>Developing pre-silicon hardware verification enviroments for automotive SoCs</p>
                 </div>
             </div>
         </section>
