ed532f3a0dfceb0e1b95a7d50204d19dd2f58511
Alex Biba <alexbiba@gmail.com>
2022-10-04 16:37:27 -0400

    Add chrome extension

--- src/Works.js
+++ src/Works.js
@@ -5,6 +5,8 @@ export default function Works() {
         <section id="works">
             <h2 className="title">Code Samples</h2>
             <div className="content">
+                <h3><a className="project" href="https://github.com/AlexanderBiba/timezone-converter-extension">Timezone Converter Browser Extension</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/timezone-converter-extension" target="_blank"/></h3>
+                <p>A browser extension that allows conversion of natural language timestamp expressions to a user configured timezone.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/goal-keeper" target="_blank"/></h3>
                 <p>A method to increase motivation to complete daily tasks by working with a partner, React.js, Firebase DB, Material UI.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/wordle" target="_blank"/></h3>
