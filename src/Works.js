e0ea31bb7cdbd913947a742932eebeccf3088075
Alex Biba <alexbiba@gmail.com>
2022-12-26 20:27:43 -0500

    Update works

--- src/Works.js
+++ src/Works.js
@@ -10,7 +10,7 @@ export default function Works() {
                 <h3><a className="project" href="https://AlexanderBiba.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/goal-keeper" target="_blank"/></h3>
                 <p>A method to increase motivation to complete daily tasks by working with a partner, React.js, Firebase DB, Material UI.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/wordle" target="_blank"/></h3>
-                <p>A classic wordle game with a new word every day, React.js.</p>
+                <p>A classic wordle game with a new word every day, React.js., Firebase DB, Google Cloud Functions</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/">My Personal Homepage</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/AlexanderBiba.github.io" target="_blank"/></h3>
                 <p>You're already here, might as well check out the code, written in React.js.</p>
             </div>
