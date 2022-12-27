c7945d69521fc89c67eb7cc12ffba8c31ccf8d38
Alex Biba <alexbiba@gmail.com>
2022-12-26 20:29:04 -0500

    Update works

--- src/Works.js
+++ src/Works.js
@@ -7,10 +7,10 @@ export default function Works() {
             <div className="content">
                 <h3><a className="project" href="https://github.com/AlexanderBiba/timezone-converter-extension">Timezone Converter Browser Extension</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/timezone-converter-extension" target="_blank"/></h3>
                 <p>A browser extension that allows conversion of natural language timestamp expressions to a user configured timezone.</p>
-                <h3><a className="project" href="https://AlexanderBiba.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/goal-keeper" target="_blank"/></h3>
-                <p>A method to increase motivation to complete daily tasks by working with a partner, React.js, Firebase DB, Material UI.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/wordle" target="_blank"/></h3>
                 <p>A classic wordle game with a new word every day, React.js., Firebase DB, Google Cloud Functions</p>
+                <h3><a className="project" href="https://AlexanderBiba.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/goal-keeper" target="_blank"/></h3>
+                <p>Complete daily tasks by working with a partner, React.js, Firebase DB, Material UI.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/">My Personal Homepage</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/AlexanderBiba.github.io" target="_blank"/></h3>
                 <p>You're already here, might as well check out the code, written in React.js.</p>
             </div>
