3c8522ddf3d5804fddbe4f72f543b0e0003279a2
Alex Biba <alexbiba@gmail.com>
2022-12-26 20:29:49 -0500

    dot

--- src/Works.js
+++ src/Works.js
@@ -8,7 +8,7 @@ export default function Works() {
                 <h3><a className="project" href="https://github.com/AlexanderBiba/timezone-converter-extension">Timezone Converter Browser Extension</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/timezone-converter-extension" target="_blank"/></h3>
                 <p>A browser extension that allows conversion of natural language timestamp expressions to a user configured timezone.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/wordle" target="_blank"/></h3>
-                <p>A classic wordle game with a new word every day, React.js., Firebase DB, Google Cloud Functions</p>
+                <p>A classic wordle game with a new word every day, React.js., Firebase DB, Google Cloud Functions.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/goal-keeper" target="_blank"/></h3>
                 <p>Complete daily tasks by working with a partner, React.js, Firebase DB, Material UI.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/">My Personal Homepage</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/AlexanderBiba.github.io" target="_blank"/></h3>
