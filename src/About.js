ac2ca4b32a75a0005af76c183d0ef440a1de0062
Alexander Biba <alexbiba@gmail.com>
2024-04-24 16:35:06 -0400

    Update about me

--- src/About.js
+++ src/About.js
@@ -10,10 +10,25 @@ export default function About() {
       <div className="content">
         <h2>About Me</h2>
         <p>
+          Senior Software Engineer with over 10 years of experience in the tech
+          industry, working across multiple domains and engineering roles.
+        </p>
+        <p>
+          My journey started with hardware VLSI design, transitioned into
+          software backend development, and most recently focused on full-stack
+          web development.
+        </p>
+        <p>
+          I am deeply passionate about technology and have a keen attention to
+          detail. I always strive to adhere to best practices, building scalable
+          solutions for the long term, and ensuring a superior user experience
+          for the end user.
+        </p>
+        <p>
           I love building cool software and developing web applications, if you
           have an interesting project, feel free to reach out!
         </p>
-        <p>I also love Video Games 🎮, Guitars 🎸, and Hiking 🥾</p>
+        <p>I also love Video Games 🎮, Music 🎸, and Hiking 🥾</p>
       </div>
     </section>
   );
