4ba585fa2813e4f1277aec1f1f4a0280c0f1603b
Alexander Biba <alexbiba@gmail.com>
2024-04-24 16:54:33 -0400

    Update skills list in Skills component

--- src/Skills.js
+++ src/Skills.js
@@ -1,33 +1,35 @@
 import "./Skills.scss";
 
 export default function Skills() {
-    return (
-        <section id="skills">
-            <h2 className="title">Skills</h2>
-            <div className="content">
-                <h3>High Proficiency</h3>
-                <ul>
-                    <li>TypeScript</li>
-                    <li>Node.js</li>
-                    <li>Python</li>
-                    <li>SQL</li>
-                    <li>Linux</li>
-                </ul>
-                <h3>Fluent With</h3>
-                <ul>
-                    <li>React.js</li>
-                    <li>Vue.js</li>
-                    <li>Groovy</li>
-                    <li>Java</li>
-                    <li>C/C++</li>
-                </ul>
-                <h3>Languages</h3>
-                <ul>
-                    <li>English - Fluent</li>
-                    <li>Hebrew - Native</li>
-                    <li>Russian - Basic</li>
-                </ul>
-            </div>
-        </section>
-    )
+  return (
+    <section id="skills">
+      <h2 className="title">Skills</h2>
+      <div className="content">
+        <h3>High Proficiency</h3>
+        <ul>
+          <li>TypeScript</li>
+          <li>Node.js</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+          <li>MySQL</li>
+          <li>Python</li>
+          <li>Linux</li>
+        </ul>
+        <h3>Fluent With</h3>
+        <ul>
+          <li>React.js</li>
+          <li>Vue.js</li>
+          <li>Groovy</li>
+          <li>Java</li>
+          <li>C/C++</li>
+        </ul>
+        <h3>Languages</h3>
+        <ul>
+          <li>English - Fluent</li>
+          <li>Hebrew - Native</li>
+          <li>Russian - Basic</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
