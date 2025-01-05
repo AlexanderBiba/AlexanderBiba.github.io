a616acc127aecbe7c4e7a9627b52223027a6c2f5
Alex Biba <alexbiba@gmail.com>
2025-01-04 19:05:46 -0500

    Update Works.js

--- src/Works.js
+++ src/Works.js
@@ -3,8 +3,10 @@ import "./Works.scss";
 export default function Works() {
     return (
         <section id="works">
-            <h2 className="title">Code Samples</h2>
+            <h2 className="title">Personal Projects</h2>
             <div className="content">
+                <h3><a className="project" href="https://github.com/AlexanderBiba/esp32-dotmatrix-wifi-clock">Wordle</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/esp32-dotmatrix-wifi-clock" target="_blank"/></h3>
+                <p>Digital Weather Clock project using an ESP32 and MAX7219 8x8 dot matrix displays to show the current time, date, and weather information.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/wordle">Wordle</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/wordle" target="_blank"/></h3>
                 <p>A classic wordle game with a new word every day, React.js., Firebase DB, Google Cloud Functions.</p>
                 <h3><a className="project" href="https://AlexanderBiba.github.io/goal-keeper">Goal Keeper</a><a className="fa fa-github" href="https://github.com/AlexanderBiba/goal-keeper" target="_blank"/></h3>
